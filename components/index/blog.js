3a9d022a01e1264dbdaaf00173dc0285698fabe2
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-04-18 01:17:50 +0300

    Update blog.js

--- components/index/blog.js
+++ components/index/blog.js
@@ -8,7 +8,6 @@ function Blog() {
   const getPost = async () => {
     const response = await fetch(
       'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/beratsblog',
-      { cache: 'reload' }
     )
     const data = await response.json()
     setData({ load: true, data: data.items.slice(0, 4), url: data.feed.link })
