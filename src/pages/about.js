a29926a13b1cd491c66f46bf407a19a42e7d4de6
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-03-29 22:10:53 -0300

    change about.js and add a style for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,19 +16,32 @@ const About = ({ data, location }) => {
         <Welcome />
         <section id="page-content">
           <p>
-            I'm a software developer
+            Welcome!
             <span role="img" aria-label="smile">
               😄
             </span>
           </p>
           <p>
-            I do machine learning
-          </p>
+          I am a machine learning engineer and a data scientist. I worked on challenging aspects of data science and machine learning, e.g.
+        </p>
+          <ul>
+            <li>
+              Tackled data including audio, images, time series, health care data, natural languages, chemical molecules
+            </li>
+            <li>
+              Maintained modular Python code with test suites (unit tests, integration, and coding style)
+            </li>
+            <li>
+              Tackled challenging scenarios, such as datasets with few labels
+            </li>
+          </ul>
+        <p>
+          I also build websites. It started as a hobby, but now I'm determined to become a full stack (Rails + JS) dev.
+        </p>
           <p>
-            I work on web
-          </p>
-          <p>
-            I'm still learning front end dev. This website is based on a template designed by Berat Bozkurt.
+            I'm still learning front end dev. This website is based on a template designed and developed by <a href="https://github.com/berat/homepage" target="_blank">
+              Berat Bozkurt
+            </a>.
           </p>
         </section>
       </Layout>
